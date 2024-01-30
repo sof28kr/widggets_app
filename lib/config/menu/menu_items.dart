2765e984822b05b87e93d086d4f84b334c477a4e
Sofia Rojas <sofia28kr@gmail.com>
2024-01-30 12:05:54 -0500

    se agrego un estilo para el appbar

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,38 @@
+
+
+import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart' show IconData;
+// hace que solo se muestre los icons del paquete
+//lo hace ligero 
+
+class MenuItem {
+  final String tittle;
+  final String Subtitle;
+  final String link;
+  final IconData icon; // control+. para llamar al constructor automatico
+
+ const MenuItem({
+    required this.tittle, 
+    required this.Subtitle, 
+    required this.link, 
+    required this.icon
+    });
+}
+
+const appMenuItems = <MenuItem>[
+
+  MenuItem(
+    tittle: 'Botones', 
+    Subtitle: 'Varios botones en Flutter', 
+    link: '/buttons', 
+    icon: Icons.smart_button_outlined
+  ),
+
+  MenuItem(
+    tittle: 'Tarjetas', 
+    Subtitle: 'Un contenedor estilizado', 
+    link: '/card', 
+    icon: Icons.credit_card
+  ),
+
+];
