5552e27a01ccb776beff7ceea2a6075bdf4cb354
Grober976 <grober.albert.huamani@gmail.com>
2024-03-05 19:00:08 -0500

    barril actualizado y screen creadas

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,9 +1,18 @@
-// es un archivo e barril 
+// es un archivo de barril
 // solo para juntar todos los imports
+
 // asi sacamos todos los imports con solo un import
-
-
+export 'package:widggets_app/presentation/screens/animated/animated_Screen.dart';
+export 'package:widggets_app/presentation/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:widggets_app/presentation/screens/botones_app/buttons_screen.dart';
 export 'package:widggets_app/presentation/screens/cards/cards_screen.dart';
 export 'package:widggets_app/presentation/screens/home/home_screen.dart';
+export 'package:widggets_app/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widggets_app/presentation/screens/progress/progress_screen.dart';
+export 'package:widggets_app/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:widggets_app/presentation/screens/ui_controls/ui_controls.dart';
 
+// seleccionamos
+//paleta de coamndos
+// sortline ascending
+//las ordena alfabeticamente
